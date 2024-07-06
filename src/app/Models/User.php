981447ebc68b29344a7f8d62baf66a189303a64b
teestojko <158604040+teestojko@users.noreply.github.com>
2024-07-07 02:28:51 +0900

    ”ショップデータ挿入”

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function shops()
+    {
+        return $this->hasMany(Shop::class);
+    }
 }
