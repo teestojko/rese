1425e181fa935470b6c8d07b86012c4ae548aa8a
teestojko <158604040+teestojko@users.noreply.github.com>
2024-08-02 00:10:09 +0900

    ”代表者作成画面遷移”

---
+++ src/resources/views/admin-dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('css')
+
+<link rel="stylesheet" href="{{ asset('css/shop-representative.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+@endsection
+
+@section('content')
+    <div class="admin-dashboard-title">Admin Dashboard</div>
+
+    <a href="{{ route('shop.edit.create') }}">Create Shop Representative</a>
+@endsection
---
+++ src/resources/views/shop-edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('css')
+
+<link rel="stylesheet" href="{{ asset('css/shop-representative.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+@endsection
+
+@section('content')
+
+@if (session('success'))
+    <p>{{ session('success') }}</p>
+@endif
+    <form method="POST" action="{{ route('shop-representative.store') }}">
+        @csrf
+        <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" required>
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <div>
+            <label for="password_confirmation">Confirm Password</label>
+            <input type="password" name="password_confirmation" id="password_confirmation" required>
+        </div>
+        <div>
+            <label for="shop_id">Shop</label>
+            <select name="shop_id" id="shop_id" required>
+                @foreach ($shops as $shop)
+                    <option value="{{ $shop->id }}">{{ $shop->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit">Create</button>
+    </form>
+@endsection
