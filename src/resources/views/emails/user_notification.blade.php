53ca83bd7bb72b7018e5d7f385765fab22ee4d8e
teestojko <158604040+teestojko@users.noreply.github.com>
2024-09-11 22:22:47 +0900

    ”リードミー修正　メール送信デザイン修正中”

--- src/resources/views/emails/user_notification.blade.php
+++ src/resources/views/emails/user_notification.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/common.css') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 @endsection
 
