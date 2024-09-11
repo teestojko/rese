e0f8307ffd045f6dc317f155f05b9e44d239088d
teestojko <158604040+teestojko@users.noreply.github.com>
2024-09-11 22:28:23 +0900

    ”メール送信デザイン修正中”

--- src/resources/views/emails/user_notification.blade.php
+++ src/resources/views/emails/user_notification.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/common.css') }}">
@@ -9,4 +9,16 @@
     <p>
         {{ $messageContent }}
     </p>
-@endsection
+@endsection --}}
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <p>
+        {{ $messageContent }}
+    </p>
+</body>
+</html>
